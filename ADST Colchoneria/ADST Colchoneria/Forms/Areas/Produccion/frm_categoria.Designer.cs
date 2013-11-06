df12e8409ab47b257ad24b31a37f7fe1d9d2f5e8
Ingeniero-JuanCarlos <juan.halo2@hotmail.com>
2013-11-06 17:36:26 -0600

    frm_categoria
    formulario de categoria

--- ADST Colchoneria/ADST Colchoneria/Forms/Areas/Produccion/frm_categoria.Designer.cs
+++ ADST Colchoneria/ADST Colchoneria/Forms/Areas/Produccion/frm_categoria.Designer.cs
@@ -28,85 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.barra1 = new Navegador.Barra();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.barra1 = new Navegador.Barra();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // barra1
+            // 
+            this.barra1.BackColor = System.Drawing.Color.Transparent;
+            this.barra1.Location = new System.Drawing.Point(4, 11);
+            this.barra1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.barra1.Name = "barra1";
+            this.barra1.Size = new System.Drawing.Size(268, 42);
+            this.barra1.TabIndex = 0;
+            this.barra1.click_nuevo_button += new Navegador.Barra.delegadoButton(this.nuevo);
+            this.barra1.click_guardar_button += new Navegador.Barra.delegadoButton(this.guardar);
+            this.barra1.click_editar_button += new Navegador.Barra.delegadoButton(this.editar);
+            this.barra1.click_buscar_button += new Navegador.Barra.delegadoButton(this.buscar);
+            this.barra1.click_eliminar_button += new Navegador.Barra.delegadoButton(this.eliminar);
+            this.barra1.click_actualizar_button += new Navegador.Barra.delegadoButton(this.actualizar);
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(0, 71);
+            this.tabControl1.Location = new System.Drawing.Point(4, 59);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(481, 377);
+            this.tabControl1.Size = new System.Drawing.Size(415, 309);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(473, 351);
-            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Size = new System.Drawing.Size(407, 283);
+            this.tabPage1.TabIndex = 4;
             this.tabPage1.Text = "Nuevo";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingresar nueva categoria";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.dataGridView1);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(473, 351);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(407, 283);
+            this.tabPage2.TabIndex = 5;
+            this.tabPage2.Text = "Registro";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // barra1
+            // comboBox1
             // 
-            this.barra1.BackColor = System.Drawing.Color.Transparent;
-            this.barra1.Location = new System.Drawing.Point(4, 24);
-            this.barra1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.barra1.Name = "barra1";
-            this.barra1.Size = new System.Drawing.Size(268, 42);
-            this.barra1.TabIndex = 0;
-            this.barra1.Load += new System.EventHandler(this.barra1_Load);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(146, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
             // 
-            // tabPage3
+            // label2
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(473, 351);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione categoria";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(69, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(286, 166);
+            this.dataGridView1.TabIndex = 0;
             // 
             // frm_categoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 448);
+            this.ClientSize = new System.Drawing.Size(455, 409);
             this.Controls.Add(this.barra1);
             this.Controls.Add(this.tabControl1);
             this.Name = "frm_categoria";
-            this.Text = "frm_categoria";
+            this.Text = "Categoria";
             this.Load += new System.EventHandler(this.frm_categoria_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private Navegador.Barra barra1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private Navegador.Barra barra1;
-        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- ADST Colchoneria/ADST Colchoneria/Forms/Areas/Produccion/frm_categoria.cs
+++ ADST Colchoneria/ADST Colchoneria/Forms/Areas/Produccion/frm_categoria.cs
@@ -14,6 +14,7 @@ using ADST_Colchoneria.Forms;
 using ADST_Colchoneria.Forms.Areas;
 using ADST_Colchoneria.Forms.Areas.Produccion;
 using ADST_Colchoneria.Forms.Principal;
+using Microsoft.VisualBasic;
 
 namespace ADST_Colchoneria.Forms.Areas.Produccion
 {
@@ -25,15 +26,130 @@ namespace ADST_Colchoneria.Forms.Areas.Produccion
             InitializeComponent();
         }
 
-        private void barra1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void frm_categoria_Load(object sender, EventArgs e)
         {
+            nuevo();          
+        }
+
+        public void nuevo()
+        {
+            tabControl1.SelectedTab = tabPage1;
+
+        }
+        public void guardar()
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Ingrese un nombre");
+                textBox1.Clear();
+            }
+            else
+            {
+                string query2 = "select * from tbm_categoria where nombre_categoria='" + textBox1.Text + "'";
+                System.Collections.ArrayList array = con.consultar(query2);
+                int intamanoarray = 0;
+                intamanoarray = array.Count;
+
+                String nombre = textBox1.Text;
+
+                if (intamanoarray != 0)
+                {
+                    MessageBox.Show("La categoria ya existe");
+                    textBox1.Clear();
+                }
+                else
+                {
+                    string tabla = "tbm_categoria";
+                    Dictionary<string, string> dict = new Dictionary<string, string>();
+                    dict.Add("nombre_categoria", textBox1.Text);
+                    con.insertar(tabla, dict);
+                    MessageBox.Show("Categoria creada exitosamente");
+                    textBox1.Clear();
+                    
+                }
+            }
 
         }
 
+        public void buscar()
+        {
+            comboBox();
+            tabControl1.SelectedTab = tabPage2;
+            string consulta = "select id_categoria as codigo, nombre_categoria as Categoria from tbm_categoria";
+            dataGridView1.DataSource = con.consulta_DataGridView(consulta);
+        }
+
+        public void comboBox()
+        {
+           /*string consulta1 = "select 	id_categoria, nombre_categoria from tbm_categoria";
+            comboBox1.DataSource = con.consulta_ComboBox(consulta1);
+            comboBox1.DisplayMember = "nombre_categoria";
+            comboBox1.ValueMember = "id_categoria"; */
+    
+        }
+
+        private void eliminar()
+        {
+
+            string tabla = "tbm_categoria";
+            string condicion = "id_categoria=" + comboBox1.SelectedValue;
+            con.eliminar(tabla, condicion);
+            MessageBox.Show("Categoria eliminada exitosamente");
+
+        }
+
+        private void editar()
+        {
+           string editado = Microsoft.VisualBasic.Interaction.InputBox("Ingresa un Nombre:");
+
+
+           if (editado == "")
+           {
+               MessageBox.Show("Ingrese un nombre");
+               textBox1.Clear();
+           }
+           else
+           {
+
+               string query2 = "select * from tbm_categoria where nombre_categoria='" + editado + "'";
+               System.Collections.ArrayList array = con.consultar(query2);
+               int intamanoarray = 0;
+               intamanoarray = array.Count;
+
+               String nombre = editado;
+
+               if (intamanoarray != 0)
+               {
+                   MessageBox.Show("La categoria ya existe");
+                   textBox1.Clear();
+               }
+
+               else
+               {
+                   string tabla = "tbm_categoria";
+                   Dictionary<string, string> dict = new Dictionary<string, string>();
+                   dict.Add("nombre_categoria", editado);
+                   string condicion = "id_categoria=" + comboBox1.SelectedValue;
+                   con.actualizar(tabla, dict, condicion);
+                   MessageBox.Show("Categoria modificada exitosamente");
+
+               }
+           }
+
+        }
+
+        private void actualizar()
+        {
+            buscar();
+            //comboBox();
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string consulta1 = "select 	id_categoria, nombre_categoria from tbm_categoria";
+            comboBox1.DataSource = con.consulta_ComboBox(consulta1);
+            comboBox1.DisplayMember = "nombre_categoria";
+            comboBox1.ValueMember = "id_categoria"; 
+        }
     }
 }
