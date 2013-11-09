1ebb4e5d166ebcd98cf9d92058dde5a4fedf74b7
HdavidG <h_david77@hotmail.com>
2013-11-09 09:22:34 -0600

    Control ausencias

---
+++ ADST Colchoneria/ADST Colchoneria/Forms/Areas/RRHH/frm_puesto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ADST_Colchoneria.Forms.Areas.RRHH
+{
+    public partial class frm_puesto : Form
+    {
+        public frm_puesto()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_puesto_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
